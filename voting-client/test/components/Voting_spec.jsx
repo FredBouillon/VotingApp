a7a3cfcbd11cb5efe5945a91f1ae0f97a836db89
Frederik Bouillon <frederikbouillon@gmail.com>
2015-11-04 22:04:24 +0100

    Added Vote and Winner component as childs of Voting + unit tests

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react/addons';
+import ReactDom from 'react-dom';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
@@ -28,6 +29,38 @@ describe('Voting', () => {
 		Simulate.click(buttons[0]);
 
 		expect(votedWith).to.equal("Trainspotting");
+	});
+
+	it('should disable buttons when user has voted', () => {
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]} hasVoted="Trainspotting" />
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		expect(buttons.length).to.equal(2);
+		expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+		expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+	});
+
+	it('should add label to the voted entry', () => {
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]} hasVoted="Trainspotting" />
+		);	
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		expect(buttons[0].textContent).to.contain('Voted');
+	});
+
+	it('should render just the winner when there is one', () => {
+		const component = renderIntoDocument(
+			<Voting winner="Trainspotting" />
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+		expect(buttons.length).to.equal(0);
+
+		const winner = ReactDom.findDOMNode(component.refs.winner);
+		expect(winner).to.be.ok;
+		expect(winner.textContent).to.contain("Trainspotting");
 	});
 
 })
